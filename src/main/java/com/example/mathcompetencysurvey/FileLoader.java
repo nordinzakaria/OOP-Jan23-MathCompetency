278d0dc79a79d14b2de21eb8fd4c7edd51005475
unknown <nordinzakaria@gmail.com>
2023-03-06 07:59:56 +0100

    setter getter for FileLoader done

--- src/main/java/com/example/mathcompetencysurvey/FileLoader.java
+++ src/main/java/com/example/mathcompetencysurvey/FileLoader.java
@@ -1,4 +1,24 @@
 package com.example.mathcompetencysurvey;
 
 public class FileLoader {
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    private String filename;
+
+    public FileLoader(String filename) {
+        this.filename = filename;
+    }
+
+    public static void main(String args[]) {
+
+        
+
+    }
+
 }
