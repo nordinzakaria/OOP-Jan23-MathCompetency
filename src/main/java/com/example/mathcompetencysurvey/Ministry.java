085b73e5cf20b916374fce18f6c08ed8c2e277e6
unknown <nordinzakaria@gmail.com>
2023-03-13 08:24:41 +0100

    added ministry name

--- src/main/java/com/example/mathcompetencysurvey/Ministry.java
+++ src/main/java/com/example/mathcompetencysurvey/Ministry.java
@@ -1,9 +1,41 @@
 package com.example.mathcompetencysurvey;
 
 public class Ministry {
+    private String ministryname;
     // attributes
     private DSchoolList schoollist;
+    private String pic;
 
-    
+    public Ministry(String pic) {
+        this.pic = pic;
+    }
 
+    public String getMinistryname() {
+        return ministryname;
+    }
+
+    public void setMinistryname(String ministryname) {
+        this.ministryname = ministryname;
+    }
+
+    public DSchoolList getSchoollist() {
+        return schoollist;
+    }
+
+    public void setSchoollist(DSchoolList schoollist) {
+        this.schoollist = schoollist;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public static void main(String args[]) {
+
+
+    }
 }
