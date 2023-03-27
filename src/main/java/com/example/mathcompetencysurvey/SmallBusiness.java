bdfb1ca42012e9091b7447b4d169c147c72f62c0
unknown <nordinzakaria@gmail.com>
2023-03-27 09:26:00 +0200

    updated

---
+++ src/main/java/com/example/mathcompetencysurvey/SmallBusiness.java
@@ -0,0 +1,9 @@
+package com.example.mathcompetencysurvey;
+
+public class SmallBusiness implements EPFContributorI {
+
+    @Override
+    public float calcEPFcontribution() {
+        return 100;
+    }
+}
