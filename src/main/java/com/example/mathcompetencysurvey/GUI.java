b77ba52a4fedf234d3a715e695bdfb9ad5b812a1
unknown <nordinzakaria@gmail.com>
2023-04-03 08:24:16 +0200

    gridpane columnconstraints used

--- src/main/java/com/example/mathcompetencysurvey/GUI.java
+++ src/main/java/com/example/mathcompetencysurvey/GUI.java
@@ -5,6 +5,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -21,13 +22,17 @@ public class GUI extends Application {
         Scene scene = new Scene(root, 300, 130, Color.WHITE);
 
         GridPane gridpane = new GridPane();
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(50);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(50);
+        gridpane.getColumnConstraints().addAll(col1,col2);
 
         Label label = new Label("Year");
         gridpane.add(label, 0, 0);
 
         TextField tf = new TextField();
-        gridpane.add(label, 1, 0);
-
+        gridpane.add(tf, 1, 0);
 
         root.getChildren().add(gridpane);
         primaryStage.setScene(scene);
